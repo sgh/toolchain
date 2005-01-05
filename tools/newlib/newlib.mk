d2a39ff824c339107dbab61d58e43e6a81b01cb8
Søren Holm <sgh@sgh.dk>
2005-01-05 00:38:57 +0000

    version bump

--- tools/newlib/newlib.mk
+++ tools/newlib/newlib.mk
@@ -3,7 +3,7 @@
 # newlib
 #
 #############################################################
-NEWLIB_VER:=1.12.0
+NEWLIB_VER:=1.13.0
 NEWLIB_SOURCE:=newlib-$(NEWLIB_VER).tar.gz
 NEWLIB_SITE:=ftp://sources.redhat.com/pub/newlib/
 NEWLIB_DIR:=$(BUILD_DIR)/newlib-$(NEWLIB_VER)
