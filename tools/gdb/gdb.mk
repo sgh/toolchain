e68e058d000eb99ff69b7ad40cd227568cb55110
Søren Holm <sgh@sgh.dk>
2005-01-05 21:08:21 +0000

    + gdb

---
+++ tools/gdb/gdb.mk
@@ -0,0 +1,11 @@
+#############################################################
+#
+# newlib
+#
+#############################################################
+GDB_VER:=6.3
+GDB_SOURCE:=gdb-$(GDB_VER).tar.bz2
+GDB_SITE:=http://ftp.gnu.org/gnu/gdb
+GDB_DIR:=$(BUILD_DIR)/gdb-$(GDB_VER)
+GDB_CAT:=bzcat
+
