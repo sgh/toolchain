9659d7101fce8a06fe8bcc0216abb3cf5a438e56
Søren Holm <sgh@sgh.dk>
2005-04-24 09:58:21 +0000

    GCC 4.0.0

--- tools/gcc/gcc.mk
+++ tools/gcc/gcc.mk
@@ -3,9 +3,9 @@
 # gcc
 #
 #############################################################
-GCC_VER:=3.4.3
+GCC_VER:=4.0.0
 GCC_SOURCE:=gcc-$(GCC_VER).tar.bz2
-GCC_SITE:=http://mirrors.sunsite.dk/gnu/gcc/gcc-$(GCC_VER)/
+GCC_SITE:=ftp://ftp.gnu.org/gnu/gcc/gcc-$(GCC_VER)/
 GCC_DIR:=$(BUILD_DIR)/gcc-$(GCC_VER)
 GCC_CAT:=bzcat
 
