a5d48464d0b5cc1dd6ceacafbdd72453bc1a168f
Søren Holm <sgh@sgh.dk>
2006-09-29 09:44:14 +0000

    binutils 2.17

--- tools/binutils/binutils.mk
+++ tools/binutils/binutils.mk
@@ -3,7 +3,7 @@
 # binutils 
 #
 #############################################################
-BINUTILS_VER:=2.16.1
+BINUTILS_VER:=2.17
 BINUTILS_SOURCE:=binutils-$(BINUTILS_VER).tar.bz2
 BINUTILS_SITE:=http://ftp.gnu.org/gnu/binutils/
 BINUTILS_DIR:=$(BUILD_DIR)/binutils-$(BINUTILS_VER)
