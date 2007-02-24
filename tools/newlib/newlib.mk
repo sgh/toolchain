7645f87e9190e5b4dfbb534e0e0f658175f2f9df
Søren Holm <sgh@sgh.dk>
2007-02-24 20:43:10 +0000

    updated newlib

--- tools/newlib/newlib.mk
+++ tools/newlib/newlib.mk
@@ -3,7 +3,7 @@
 # newlib
 #
 #############################################################
-NEWLIB_VER:=1.14.0
+NEWLIB_VER:=1.15.0
 NEWLIB_SOURCE:=newlib-$(NEWLIB_VER).tar.gz
 NEWLIB_SITE:=ftp://sources.redhat.com/pub/newlib/
 NEWLIB_DIR:=$(BUILD_DIR)/newlib-$(NEWLIB_VER)
