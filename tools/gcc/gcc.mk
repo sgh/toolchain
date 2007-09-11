a19834c38cb73ee2357a8d7c1821cce98a18dd1f
Søren Holm <sgh@sgh.dk>
2007-09-11 21:59:26 +0000

    newer tools fail in several ways

--- tools/gcc/gcc.mk
+++ tools/gcc/gcc.mk
@@ -3,7 +3,7 @@
 # gcc
 #
 #############################################################
-GCC_VER:=4.2.1
+GCC_VER:=4.2.0
 GCC_SOURCE:=gcc-$(GCC_VER).tar.bz2
 GCC_SITE:=ftp://ftp.gnu.org/gnu/gcc/gcc-$(GCC_VER)/
 GCC_DIR:=$(BUILD_DIR)/gcc-$(GCC_VER)
