943e40f93c3b6f122c9f96b745144d0133e9c16c
Søren Holm <sgh@sgh.dk>
2008-02-10 02:28:51 +0000

    gdb 6.7.1

--- tools/gdb/gdb.mk
+++ tools/gdb/gdb.mk
@@ -3,7 +3,7 @@
 # newlib
 #
 #############################################################
-GDB_VER:=6.6
+GDB_VER:=6.7.1
 GDB_SOURCE:=gdb-$(GDB_VER).tar.bz2
 GDB_SITE:=http://ftp.gnu.org/gnu/gdb
 GDB_DIR:=$(BUILD_DIR)/gdb-$(GDB_VER)
