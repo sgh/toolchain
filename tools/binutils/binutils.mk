1d7b2194b29a830f8b005188df82d18472845668
Søren Holm <sgh@sgh.dk>
2009-02-10 21:58:04 +0000

    updated to binutils 2.19.1

--- tools/binutils/binutils.mk
+++ tools/binutils/binutils.mk
@@ -3,7 +3,7 @@
 # binutils 
 #
 #############################################################
-BINUTILS_VER:=2.19
+BINUTILS_VER:=2.19.1
 BINUTILS_SOURCE:=binutils-$(BINUTILS_VER).tar.bz2
 BINUTILS_SITE:=http://ftp.gnu.org/gnu/binutils/
 BINUTILS_DIR:=$(BUILD_DIR)/binutils-$(BINUTILS_VER)
