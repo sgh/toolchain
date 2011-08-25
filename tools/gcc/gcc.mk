7ecd9205ce99f6e80d0904ba97bd1af3a705678e
Søren Holm <sh@mikrofyn.com>
2011-08-25 13:09:06 +0200

    local build

--- tools/gcc/gcc.mk
+++ tools/gcc/gcc.mk
@@ -3,9 +3,9 @@
 # gcc
 #
 #############################################################
-GCC_VER:=4.6.1
+GCC_VER:=snapshot
 GCC_SOURCE:=gcc-$(GCC_VER).tar.bz2
-GCC_SITE:=ftp://ftp.gnu.org/gnu/gcc/gcc-$(GCC_VER)/
+#GCC_SITE:=ftp://ftp.gnu.org/gnu/gcc/gcc-$(GCC_VER)/
+GCC_SITE:=http://localhost/~sgh
 GCC_DIR:=$(BUILD_DIR)/gcc-$(GCC_VER)
 GCC_CAT:=bzcat
-
