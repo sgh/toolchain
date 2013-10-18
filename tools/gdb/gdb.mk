698c5d7838e110edf8f3d0e459b02ea02476f941
Søren Holm <sgh@sgh.dk>
2013-10-18 03:06:42 +0200

    Bumped GCC to 4.8.2 and GDB to 7.6.1

--- tools/gdb/gdb.mk
+++ tools/gdb/gdb.mk
@@ -3,7 +3,7 @@
 # newlib
 #
 #############################################################
-GDB_VER:=7.5.1
+GDB_VER:=7.6.1
 GDB_SOURCE:=gdb-$(GDB_VER).tar.bz2
 GDB_SITE:=http://ftp.gnu.org/gnu/gdb
 GDB_DIR:=$(BUILD_DIR)/gdb-$(GDB_VER)
