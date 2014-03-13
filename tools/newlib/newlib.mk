437b91276e1d09885a5c46146550f67e458071f0
Søren Holm <sgh@sgh.dk>
2014-03-13 10:21:16 +0100

    Updated newlib.

--- tools/newlib/newlib.mk
+++ tools/newlib/newlib.mk
@@ -3,7 +3,7 @@
 # newlib
 #
 #############################################################
-NEWLIB_VER:=2.0.0
+NEWLIB_VER:=2.1.0
 NEWLIB_SOURCE:=newlib-$(NEWLIB_VER).tar.gz
 NEWLIB_SITE:=ftp://sourceware.org/pub/newlib
 NEWLIB_DIR:=$(BUILD_DIR)/newlib-$(NEWLIB_VER)
